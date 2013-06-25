e896b18ed2a8af8692698f2654ed50679b27e8d2
adieu <adieu@adieu.me>
2013-06-26 01:12:44 +0800

    Don't create hstore extension from the application since it requires superuser privilege. Leave it to the DBA.

--- django_hstore/models.py
+++ django_hstore/models.py
@@ -4,9 +4,6 @@ try:
     from .util import register_hstore
 
     def register_hstore_extension(sender, connection, *args, **kwargs):
-        cursor = connection.cursor()
-        cursor.execute("CREATE EXTENSION IF NOT EXISTS hstore")
-        connection.commit_unless_managed()
         register_hstore(connection.connection, globally=True, unicode=True)
 
     connection_created.connect(register_hstore_extension, sender=DatabaseWrapper)
